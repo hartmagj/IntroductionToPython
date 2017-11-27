b6eb190655fde49fd47245d2ad8c0013f9bac13c
hartmagj <hartmagj@rose-hulman.edu>
2017-11-27 17:11:03 -0500

    m2_hello_world is done, added semicolons to m1e because I'm mildly neurotic

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Grant Hartman.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,48 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg;
+window = rg.TurtleWindow();
+
+black_turtle = rg.SimpleTurtle("turtle");
+blue_turtle = rg.SimpleTurtle("turtle");
+red_turtle = rg.SimpleTurtle("turtle");
+
+black_turtle.pen = rg.Pen("black", 3);
+blue_turtle.pen = rg.Pen("blue", 3);
+red_turtle.pen = rg.Pen("red", 3);
+
+black_turtle.speed = 100;
+blue_turtle.speed = 100;
+red_turtle.speed = 100;
+
+bSize = 0;
+
+for k in range(30):
+    black_turtle.draw_regular_polygon(3,bSize + (k*10));
+
+    black_turtle.pen_up();
+    black_turtle.forward(bSize + k*5);
+    black_turtle.right(20);
+
+    black_turtle.pen_down();
+
+for k in range(30):
+    blue_turtle.draw_regular_polygon(4,bSize + (k*10));
+
+    blue_turtle.pen_up();
+    blue_turtle.forward(bSize + k*5);
+    blue_turtle.right(20);
+
+    blue_turtle.pen_down();
+
+for k in range(30):
+    red_turtle.draw_regular_polygon(5,bSize + (k*10));
+
+    red_turtle.pen_up();
+    red_turtle.forward(bSize + k*5);
+    red_turtle.right(20);
+
+    red_turtle.pen_down();
+
+window.close_on_mouse_click()
