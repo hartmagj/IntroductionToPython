5879d926b16955df9e7904798cf32e750cf453cc
hartmagj <hartmagj@rose-hulman.edu>
2017-11-27 16:05:40 -0500

    m2_hello_world is done, added semicolons to m1e because I'm mildly neurotic

--- src/m1e_comments_strings_print.py
+++ src/m1e_comments_strings_print.py
@@ -9,8 +9,8 @@
 #
 ########################################################################
 
-print('Hello, World')
-print('hi there')
-print('one', 'two', 'buckle my shoe')
-print(3 + 9)
-print('3 + 9', 'versus', 3 + 9)
+print('Hello, World');
+print('hi there');
+print('one', 'two', 'buckle my shoe');
+print(3 + 9);
+print('3 + 9', 'versus', 3 + 9);
